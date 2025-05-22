63c007c71753da8906ba8bcf853481948b6f04ae
Aniket Tikudave <anikettikudave187@gmail.com>
2025-05-22 12:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
-  // your code here
+  let result="";
+	for(let i=0;i<s.length;i++){
+		const char=s[i];
+		if(char==' '){
+			return result;
+		}
+		result+=char;
+	}
+	return result;
 }
 
 // Do not change the code below
